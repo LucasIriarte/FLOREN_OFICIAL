0f25586b96198fe947dc7fb50e2e554e39e5ec6d
LucasIriarte <lucasiriarte67@gmail.com>
2024-06-02 23:56:08 -0300

    add: Header component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
-
+import { Header } from "../components/Header/Header.jsx"
 
 
 
 export const Home = () => {
     return (
         <>
+            <Header />
             <h1>This is a home!</h1>
         </>
     )
