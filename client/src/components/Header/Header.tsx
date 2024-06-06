54f4c536007ad7576cb62d7ede3b0b1ba47bd9af
LucasIriarte <lucasiriarte67@gmail.com>
2024-06-05 23:33:11 -0300

    change: proof header in tsx

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,9 +1,11 @@
-export const Header = () => {
+import React from 'react';
+
+const Header: React.FC = () => {
     return (
-        <>
         <div className="bg-black text-white font-bold text-2xl">
             This is a header!
         </div>
-        </>
-    )
-}
+    );
+};
+
+export default Header;
